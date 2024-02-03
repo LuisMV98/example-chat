59836340a8aaf04bc1171de1f7f5485e010c14eb
Luis Moreno <luis1071@live.com.mx>
2024-02-03 01:29:23 -0600

    Mostrar en tiempo real los mensajes enviados, saber si está en linea un usario y si está escribiendo

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,36 +6,127 @@ const chatWith = get(".chatWith");
 const typing = get(".typing");
 const chatStatus = get(".chatStatus");
 const chatId = window.location.pathname.substr(6);
+let authUser;
+let typingTimer = false;
 
-    window.onload = function () {
+window.onload = function () {
 
-      axios.get('/auth/user')
-      .then( res => {
+  axios.get('/auth/user')
+  .then( res => {
 
-        authUser = res.data.authUser;
+    authUser = res.data.authUser;
 
-      }).then(() => {
+  })
+  .then(() => {
 
-        axios.get(`/chat/${chatId}/get_users`).then( res => {
-    
-          let results = res.data.users.filter( user => user.id != authUser.id);
-    
-          if(results.length > 0)
-            chatWith.innerHTML = results[0].name;
-    
-        });
-    
-      }).then(() => {
-    
-        axios.get(`/chat/${chatId}/get_messages`).then(res => {
-    
-          appendMessages(res.data.messages);
-    
-        });
-    
+    axios.get(`/chat/${chatId}/get_users`).then( res => {
+
+      let results = res.data.users.filter( user => user.id != authUser.id);
+
+      if(results.length > 0)
+        chatWith.innerHTML = results[0].name;
+
+    });
+
+  })
+  .then(() => {
+
+    axios.get(`/chat/${chatId}/get_messages`).then(res => {
+
+      appendMessages(res.data.messages);
+
+    });
+
+  })
+  .then(() => {
+
+    Echo.join(`chat.${chatId}`)
+      .listen('MessageSent', (e) => {
+
+        appendMessage(
+          e.message.user.name,
+          PERSON_IMG,
+          'left',
+          e.message.content,
+          formatDate(new Date(e.message.created_at))
+        );
+
       })
+      .here(users => {
 
-    }
+        let result = users.filter(user => user.id != authUser.id);
+
+          if(result.length > 0)
+            chatStatus.className = 'chatStatus online';
+
+      })
+      .joining(user => {
+
+        if(user.id != authUser.id)
+          chatStatus.className = 'chatStatus online';
+
+      })
+      .leaving(user => {
+
+        if(user.id != authUser.id)
+          chatStatus.className = 'chatStatus offline';
+
+      })
+      .listenForWhisper('typing', e => {
+
+        if(e > 0)
+          typing.style.display = '';
+
+        if(typingTimer){
+          clearTimeout(typingTimer);
+        }
+
+        typingTimer = setTimeout( () => {
+
+          typing.style.display = 'none';
+
+          typingTimer = false;
+
+        }, 3000);
+
+      });
+
+  });
+
+}
+
+msgerForm.addEventListener("submit", event => {
+
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+
+  if (!msgText) return;
+
+  axios.post('/message/sent', {
+    message: msgText,
+    chat_id: chatId
+  }).then( res => {
+
+    let data = res.data;
+
+    appendMessage(
+      data.user.name,
+      PERSON_IMG,
+      'right',
+      data.content,
+      formatDate(new Date(data.created_at))
+    );
+
+  }).catch( error => {
+
+    console.log('Ha ocurrido un error');
+    console.log(error);
+
+  });
+
+  msgerInput.value = "";
+});
 
 msgerForm.addEventListener("submit", event => {
 
@@ -112,11 +203,16 @@ function appendMessage(name, img, side, text, date) {
     scrollToBottom();
   }
 
-//Echo
-  Echo.join(`chat.${chatId}`)
-    .listen('MessageSent', (e) => {
-        console.log(e)
-    })
+  function sendTypingEvent()
+  {
+  
+    typingTimer = true;
+  
+    Echo.join(`chat.${chatId}`)
+      .whisper('typing', msgerInput.value.length);
+  
+  }
+
 
 
 // Utils
